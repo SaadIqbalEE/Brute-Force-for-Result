893c941f565487241a38ddd8fffda2fbc10f3546
Saad Iqbal <saadiqbal0001@gmail.com>
2017-12-06 20:44:53 +0500

    Complete Code

---
+++ Web_friendly/Web_friendly/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Web_friendly
+{
+    partial class App_win
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App_win));
+            this.REG_value = new System.Windows.Forms.TextBox();
+            this.Reg_label = new System.Windows.Forms.Label();
+            this.Batch_label = new System.Windows.Forms.Label();
+            this.GO_b = new System.Windows.Forms.Button();
+            this.Birth_label = new System.Windows.Forms.Label();
+            this.Birth_value = new System.Windows.Forms.TextBox();
+            this.CGPA_label = new System.Windows.Forms.Label();
+            this.CGPA_value = new System.Windows.Forms.TextBox();
+            this.WB1 = new System.Windows.Forms.WebBrowser();
+            this.URL_label = new System.Windows.Forms.Label();
+            this.example = new System.Windows.Forms.Label();
+            this.Batches = new System.Windows.Forms.ComboBox();
+            this.Error = new System.Windows.Forms.TextBox();
+            this.URL_value_box = new System.Windows.Forms.ComboBox();
+            this.Starting_Year = new System.Windows.Forms.ComboBox();
+            this.year_label = new System.Windows.Forms.Label();
+            this.example_Y = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // REG_value
+            // 
+            this.REG_value.Location = new System.Drawing.Point(165, 12);
+            this.REG_value.Name = "REG_value";
+            this.REG_value.Size = new System.Drawing.Size(226, 20);
+            this.REG_value.TabIndex = 0;
+            this.REG_value.TextChanged += new System.EventHandler(this.REG_value_TextChanged);
+            // 
+            // Reg_label
+            // 
+            this.Reg_label.AutoSize = true;
+            this.Reg_label.Location = new System.Drawing.Point(78, 15);
+            this.Reg_label.Name = "Reg_label";
+            this.Reg_label.Size = new System.Drawing.Size(81, 13);
+            this.Reg_label.TabIndex = 1;
+            this.Reg_label.Text = "Registration no.";
+            this.Reg_label.Click += new System.EventHandler(this.Reg_label_Click);
+            // 
+            // Batch_label
+            // 
+            this.Batch_label.AutoSize = true;
+            this.Batch_label.Location = new System.Drawing.Point(409, 15);
+            this.Batch_label.Name = "Batch_label";
+            this.Batch_label.Size = new System.Drawing.Size(64, 13);
+            this.Batch_label.TabIndex = 2;
+            this.Batch_label.Text = "Batch name";
+            this.Batch_label.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // GO_b
+            // 
+            this.GO_b.Location = new System.Drawing.Point(1229, 14);
+            this.GO_b.Name = "GO_b";
+            this.GO_b.Size = new System.Drawing.Size(108, 23);
+            this.GO_b.TabIndex = 4;
+            this.GO_b.Text = "GO";
+            this.GO_b.UseVisualStyleBackColor = true;
+            this.GO_b.Click += new System.EventHandler(this.GO_b_Click);
+            // 
+            // Birth_label
+            // 
+            this.Birth_label.AutoSize = true;
+            this.Birth_label.Location = new System.Drawing.Point(770, 631);
+            this.Birth_label.Name = "Birth_label";
+            this.Birth_label.Size = new System.Drawing.Size(66, 13);
+            this.Birth_label.TabIndex = 5;
+            this.Birth_label.Text = "Date of Birth";
+            this.Birth_label.Click += new System.EventHandler(this.Birth_label_Click);
+            // 
+            // Birth_value
+            // 
+            this.Birth_value.Enabled = false;
+            this.Birth_value.Location = new System.Drawing.Point(878, 628);
+            this.Birth_value.Name = "Birth_value";
+            this.Birth_value.Size = new System.Drawing.Size(158, 20);
+            this.Birth_value.TabIndex = 6;
+            this.Birth_value.TextChanged += new System.EventHandler(this.Birth_value_TextChanged);
+            // 
+            // CGPA_label
+            // 
+            this.CGPA_label.AutoSize = true;
+            this.CGPA_label.Location = new System.Drawing.Point(368, 631);
+            this.CGPA_label.Name = "CGPA_label";
+            this.CGPA_label.Size = new System.Drawing.Size(36, 13);
+            this.CGPA_label.TabIndex = 7;
+            this.CGPA_label.Text = "CGPA";
+            this.CGPA_label.Click += new System.EventHandler(this.CGPA_label_Click);
+            // 
+            // CGPA_value
+            // 
+            this.CGPA_value.Enabled = false;
+            this.CGPA_value.Location = new System.Drawing.Point(442, 628);
+            this.CGPA_value.Name = "CGPA_value";
+            this.CGPA_value.Size = new System.Drawing.Size(100, 20);
+            this.CGPA_value.TabIndex = 8;
+            this.CGPA_value.TextChanged += new System.EventHandler(this.CGPA_value_TextChanged);
+            // 
+            // WB1
+            // 
+            this.WB1.Location = new System.Drawing.Point(81, 56);
+            this.WB1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WB1.Name = "WB1";
+            this.WB1.ScriptErrorsSuppressed = true;
+            this.WB1.Size = new System.Drawing.Size(1200, 540);
+            this.WB1.TabIndex = 9;
+            this.WB1.Url = new System.Uri("", System.UriKind.Relative);
+            this.WB1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WB1_DocumentCompleted);
+            // 
+            // URL_label
+            // 
+            this.URL_label.AutoSize = true;
+            this.URL_label.Location = new System.Drawing.Point(631, 15);
+            this.URL_label.Name = "URL_label";
+            this.URL_label.Size = new System.Drawing.Size(29, 13);
+            this.URL_label.TabIndex = 10;
+            this.URL_label.Text = "URL";
+            this.URL_label.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // example
+            // 
+            this.example.AutoSize = true;
+            this.example.Location = new System.Drawing.Point(642, 36);
+            this.example.Name = "example";
+            this.example.Size = new System.Drawing.Size(331, 13);
+            this.example.TabIndex = 12;
+            this.example.Text = "URL i.e. http://seecs.nust.edu.pk/Student-Affairs/Results/fall-2015/";
+            this.example.Click += new System.EventHandler(this.example_Click);
+            // 
+            // Batches
+            // 
+            this.Batches.FormattingEnabled = true;
+            this.Batches.Location = new System.Drawing.Point(479, 12);
+            this.Batches.Name = "Batches";
+            this.Batches.Size = new System.Drawing.Size(130, 21);
+            this.Batches.TabIndex = 14;
+            this.Batches.SelectedIndexChanged += new System.EventHandler(this.Batches_SelectedIndexChanged);
+            // 
+            // Error
+            // 
+            this.Error.Enabled = false;
+            this.Error.Location = new System.Drawing.Point(81, 602);
+            this.Error.Name = "Error";
+            this.Error.Size = new System.Drawing.Size(1200, 20);
+            this.Error.TabIndex = 15;
+            this.Error.TextChanged += new System.EventHandler(this.Error_TextChanged);
+            // 
+            // URL_value_box
+            // 
+            this.URL_value_box.FormattingEnabled = true;
+            this.URL_value_box.Location = new System.Drawing.Point(677, 12);
+            this.URL_value_box.Name = "URL_value_box";
+            this.URL_value_box.Size = new System.Drawing.Size(305, 21);
+            this.URL_value_box.TabIndex = 16;
+            this.URL_value_box.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Starting_Year
+            // 
+            this.Starting_Year.FormattingEnabled = true;
+            this.Starting_Year.Location = new System.Drawing.Point(1099, 12);
+            this.Starting_Year.Name = "Starting_Year";
+            this.Starting_Year.Size = new System.Drawing.Size(63, 21);
+            this.Starting_Year.TabIndex = 17;
+            this.Starting_Year.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // year_label
+            // 
+            this.year_label.AllowDrop = true;
+            this.year_label.AutoSize = true;
+            this.year_label.Location = new System.Drawing.Point(1018, 19);
+            this.year_label.Name = "year_label";
+            this.year_label.Size = new System.Drawing.Size(65, 13);
+            this.year_label.TabIndex = 18;
+            this.year_label.Text = "Sarting Year";
+            this.year_label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // example_Y
+            // 
+            this.example_Y.AutoSize = true;
+            this.example_Y.Location = new System.Drawing.Point(1046, 36);
+            this.example_Y.Name = "example_Y";
+            this.example_Y.Size = new System.Drawing.Size(137, 13);
+            this.example_Y.TabIndex = 19;
+            this.example_Y.Text = "i.e. 94, checks for 94,95,96";
+            this.example_Y.Click += new System.EventHandler(this.example_Y_Click);
+            // 
+            // App_win
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1354, 681);
+            this.Controls.Add(this.example_Y);
+            this.Controls.Add(this.year_label);
+            this.Controls.Add(this.Starting_Year);
+            this.Controls.Add(this.URL_value_box);
+            this.Controls.Add(this.Error);
+            this.Controls.Add(this.Batches);
+            this.Controls.Add(this.example);
+            this.Controls.Add(this.URL_label);
+            this.Controls.Add(this.WB1);
+            this.Controls.Add(this.CGPA_value);
+            this.Controls.Add(this.CGPA_label);
+            this.Controls.Add(this.Birth_value);
+            this.Controls.Add(this.Birth_label);
+            this.Controls.Add(this.GO_b);
+            this.Controls.Add(this.Batch_label);
+            this.Controls.Add(this.Reg_label);
+            this.Controls.Add(this.REG_value);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "App_win";
+            this.Text = "GPA_win";
+            this.Load += new System.EventHandler(this.App_win_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox REG_value;
+        private System.Windows.Forms.Label Reg_label;
+        private System.Windows.Forms.Label Batch_label;
+        private System.Windows.Forms.ComboBox Batches;
+        private System.Windows.Forms.Button GO_b;
+        private System.Windows.Forms.Label Birth_label;
+        private System.Windows.Forms.TextBox Birth_value;
+        private System.Windows.Forms.Label CGPA_label;
+        private System.Windows.Forms.TextBox CGPA_value;
+        private System.Windows.Forms.WebBrowser WB1;
+        private System.Windows.Forms.Label URL_label;
+        private System.Windows.Forms.ComboBox URL_value_box;
+        private System.Windows.Forms.Label example;
+        private System.Windows.Forms.TextBox Error;
+        private System.Windows.Forms.ComboBox Starting_Year;
+        private System.Windows.Forms.Label year_label;
+        private System.Windows.Forms.Label example_Y;
+        
+    }
+}
+
---
+++ Web_friendly/Web_friendly/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Web_friendly
+{
+    public partial class App_win : Form
+    {
+        public static int C_Date;
+        public static int C_month;
+        public static int C_year;
+        public static int last_year;
+        public bool check_1 ;
+        public bool found_1 ;
+
+        public void next_date() {
+            if (C_Date < 31)
+            {
+                C_Date += 1;
+            }
+            else {
+                C_Date = 1;
+                if (C_month < 12)
+                {
+                    C_month += 1;
+                }
+                else {
+                    C_month = 1;
+                    C_year += 1;
+                }
+            }
+        }
+
+        public App_win()
+        {
+            InitializeComponent();
+            this.CGPA_value.Text = "0.00";
+            this.Birth_value.Text = "1/1/1900";
+            this.WB1.Navigate("www.google.com");
+            Batches.Items.Add("BE(SE)-3");
+            Batches.Items.Add("BE(SE)-4");
+            Batches.Items.Add("BE(SE)-5");
+            Batches.Items.Add("BE(SE)-6");
+            Batches.Items.Add("BEE-4");
+            Batches.Items.Add("BEE-5");
+            Batches.Items.Add("BEE-6");
+            Batches.Items.Add("BEE-7");
+            Batches.Items.Add("BS(CS)-2");
+            Batches.Items.Add("BS(CS)-3");
+            Batches.Items.Add("BS(CS)-4");
+            Batches.Items.Add("BS(CS)-5");
+            Batches.Items.Add("MS(IT)");
+            Batches.Items.Add("MS(CS)");
+            Batches.Items.Add("MS(IS)");
+            Batches.Items.Add("MS(ITE)");
+            Batches.Items.Add("MS(EE)-TCN");
+            Batches.Items.Add("MS(EE)-DSP");
+            Batches.Items.Add("MS(EE)-RF");
+            Batches.Items.Add("MS(EE)-PEC");
+            for (int i = 54; i <= 97; i++)
+            {
+                Starting_Year.Items.Add(i.ToString());
+            }
+            URL_value_box.Items.Add("http://seecs.nust.edu.pk/Student-Affairs/Results/fall-2015/");
+            C_Date = 1;
+            C_month = 1;
+            check_1 = true;
+            found_1 = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void GO_b_Click(object sender, EventArgs e)
+        {
+            GO_b.Text = "Working...";
+            //all go code here
+            if (Batches.SelectedItem == null || string.IsNullOrEmpty(REG_value.Text)
+                || URL_value_box.SelectedItem == null || Starting_Year.SelectedItem == null)
+            {
+                Error.Text = "Some information is missing...check if any data-box in empty";
+            }
+            else
+            {
+                Error.Text = "Proceeding.....";
+                C_year = Int32.Parse(Starting_Year.Text);
+                last_year = Int32.Parse(Starting_Year.Text) + 2;
+                while (C_year <= last_year && !found_1)
+                {
+                    WB1.Navigate(URL_value_box.Text);
+                    while (WB1.ReadyState != WebBrowserReadyState.Complete)
+                    {
+                        Application.DoEvents();
+                    }
+                    Error.Text = "Loaded page";
+                    WB1.Document.GetElementById("regno").SetAttribute("value", REG_value.Text);
+                    WB1.Document.GetElementById("cboclass").SetAttribute("value", Batches.Text);
+                    WB1.Document.GetElementById("cboyear1").SetAttribute("value", C_year.ToString());
+                    if (C_Date < 10)
+                    {
+                        WB1.Document.GetElementById("cboday1").SetAttribute("value", "0" + C_Date.ToString());
+                    }
+                    else
+                    {
+                        WB1.Document.GetElementById("cboday1").SetAttribute("value", C_Date.ToString());
+                    }
+                    if (C_month < 10)
+                    {
+                        WB1.Document.GetElementById("cbomonth1").SetAttribute("value", "0" + C_month.ToString());
+                    }
+                    else
+                    {
+                        WB1.Document.GetElementById("cbomonth1").SetAttribute("value", C_month.ToString());
+                    }
+                    WB1.Document.GetElementById("Submit").InvokeMember("click");
+                    while (check_1)
+                    {
+                        Application.DoEvents();
+                        if (WB1.Url.ToString() == "http://seecs.nust.edu.pk/Student-Affairs/Results/fall-2015/result.php") {
+                            string file_all = WB1.DocumentText;
+                            string[] separators = { "<b>", "</b>", REG_value.Text };
+                            string[] words = file_all.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                            if (words.Length >= 2)
+                                if (words[1] == "Date of Birth is not matched with provided Registration No, unable to display result")
+                                {
+                                    check_1 = false;
+                                }
+                                else
+                                {
+                                    if(words.Length ==2){
+                                    found_1 = true;
+                                    check_1 = false;
+                                    }
+                                }
+                                
+                        }
+                    }
+                    if (!found_1)
+                    {
+                        next_date();
+                    }
+                    check_1 = true;
+                }
+                if (found_1)
+                {
+                    Error.Text = "FOUND";
+                    CGPA_value.Text="On webpage";
+                    Birth_value.Text = C_Date.ToString()+"/"+C_month.ToString()+ "/"+C_year.ToString();
+                    found_1 = false;
+                }
+                else {
+                    Error.Text = "notfound";
+                }
+                C_Date = 1;
+                C_month = 1;
+                Error.Text = "Done";
+            }
+            GO_b.Text = "GO";
+        }
+        private void WB1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+        }
+
+        private void Birth_value_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CGPA_value_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+        private void REG_value_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Reg_label_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void example_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CGPA_label_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Birth_label_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void Batches_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Error_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void App_win_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void example_Y_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
